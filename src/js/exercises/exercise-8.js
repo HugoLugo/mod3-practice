c4e972c27a3ac806054548b29c6909ec40dff210
Hugo Lugo <hugolugoesperilla@gmail.com>
2023-10-29 21:54:13 +0100

    Ejercicio8

--- src/js/exercises/exercise-8.js
+++ src/js/exercises/exercise-8.js
@@ -5,6 +5,27 @@ que ocupa cada carácter en el abecedario. Para que sea más sencillo, elimina l
 (recuerda que puedes utilizar las funciones de ejercicios anteriores).
 */
 
-const alphabet = "abcdefghijklmnopqrstuvwxyz";
+function limpiarCadena(cadena) {
+    return cadena.replace(/ /g, "").normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+}
 
-const abc = "abc";
+function posicionEnAbecedario(letra) {
+    letra = letra.toLowerCase();
+    const codigoA = 'a'.charCodeAt(0);
+    return letra.charCodeAt(0) - codigoA + 1;
+}
+
+function posicionesEnAbecedario(cadena) {
+    const cadenaLimpia = limpiarCadena(cadena);
+    const posiciones = [];
+    for (let i = 0; i < cadenaLimpia.length; i++) {
+        const letra = cadenaLimpia[i];
+        const posicion = posicionEnAbecedario(letra);
+        posiciones.push(posicion);
+    }
+    return posiciones;
+}
+
+const texto = "Hola, mundo!";
+const posiciones = posicionesEnAbecedario(texto);
+console.log(posiciones);

1e225fa504d316d117d55b1b71b9c2bb3dcb98da
Hugo Lugo <hugolugoesperilla@gmail.com>
2023-10-29 22:29:06 +0100

    Ejercicio20

--- src/js/exercises/exercise-20.js
+++ src/js/exercises/exercise-20.js
@@ -12,3 +12,38 @@ Calcula el Fib de un número pasado como parámetro y muestra un mensaje:
 
 Nota: evita calcular números altos.
 */
+
+function fibonacci(n) {
+    if (n < 0) {
+      return "El número de Fibonacci no está definido para valores negativos.";
+    }
+  
+    if (n === 0) {
+      return "El primer número de la serie de Fibonacci es 0.";
+    }
+  
+    if (n === 1) {
+      return "El segundo número de la serie de Fibonacci es 1.";
+    }
+  
+    let fib1 = 0;
+    let fib2 = 1;
+    let fibN = 0;
+  
+    for (let i = 2; i <= n; i++) {
+      fibN = fib1 + fib2;
+      fib1 = fib2;
+      fib2 = fibN;
+  
+      if (fibN > 10000) {
+        return "El número de Fibonacci es demasiado grande para mostrar.";
+      }
+    }
+  
+    return `El ${n}${n === 3 ? "er" : "°"} número de la serie de Fibonacci es ${fibN}`;
+  }
+  
+  const n = 20;
+  const resultado = fibonacci(n);
+  console.log(resultado);
+  

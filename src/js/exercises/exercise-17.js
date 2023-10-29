f132d68af2ec6d27fd12430b4406dce095511755
Hugo Lugo <hugolugoesperilla@gmail.com>
2023-10-29 22:38:50 +0100

    Ejercicio17

--- src/js/exercises/exercise-17.js
+++ src/js/exercises/exercise-17.js
@@ -5,24 +5,24 @@ Dado una cadena de texto, comprueba si es un isograma o no y devuelve un mensaje
 "la cadena 'hola mundo' no es un isograma"
 */
 
-function esIsograma(cadena) {
-    cadena = cadena.replace(/ /g, "").toLowerCase();
+function esIsograma(string) {
+    string = string.replace(/ /g, "").toLowerCase();
   
     const frecuenciaLetras = {};
   
-    for (let letra of cadena) {
+    for (let letra of string) {
       if (frecuenciaLetras[letra]) {
-        return `La cadena '${cadena}' no es un isograma`;
+        return `La cadena '${string}' no es un isograma`;
       }
       frecuenciaLetras[letra] = 1;
     }
   
-    return `La cadena '${cadena}' es un isograma`;
+    return `La cadena de texto '${string}' es un isograma`;
   }
   
-  const cadena1 = "hola mundo";
-  const cadena2 = "murcielago";
+  const string1 = "hola mundo";
+  const string2 = "murcielago";
   
-  console.log(esIsograma(cadena1));
-  console.log(esIsograma(cadena2));
+  console.log(esIsograma(string1));
+  console.log(esIsograma(string2));
   

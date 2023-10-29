bb75dc54265b56e137b6485da79f772d108e4a2d
Hugo Lugo <hugolugoesperilla@gmail.com>
2023-10-29 22:07:41 +0100

    Ejercicio14

--- src/js/exercises/exercise-14.js
+++ src/js/exercises/exercise-14.js
@@ -5,3 +5,18 @@ Muestra un array que solo incluya la pelicula mejor valorada de cada uno de esto
 - Robert De Niro
 - Tom Hanks
 */
+function encontrarMejorValorada(actor) {
+    const peliculasDelActor = movies.filter((pelicula) => pelicula.actors.includes(actor));
+    if (peliculasDelActor.length === 0) {
+      return null; 
+    }
+    return peliculasDelActor.reduce((mejor, pelicula) => (pelicula.rating > mejor.rating ? pelicula : mejor));
+  }
+  
+  const actores = ["Leonardo DiCaprio", "Robert De Niro", "Tom Hanks"];
+  
+  const peliculasMejorValoradas = actores.map((actor) => encontrarMejorValorada(actor)).filter((pelicula) => pelicula !== null);
+  
+  console.log("Películas mejor valoradas de los actores:");
+  console.log(peliculasMejorValoradas);
+  

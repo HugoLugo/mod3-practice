88702790271cf4dce63bfed4cda7bfcddc6c7987
Hugo Lugo <hugolugoesperilla@gmail.com>
2023-10-29 21:18:37 +0100

    Ejercicio1

--- src/js/exercises/exercise-1.js
+++ src/js/exercises/exercise-1.js
@@ -4,8 +4,8 @@ Muestra el siguiente saludo pasando un nombre como parámetro
 “Hello <name>, how are you doing today?”
 */
 
-function greeting(name) {
-  return `Hello ${name}, how are you doing today?`;
+function saludar(nombre) {
+  console.log("Hello " + nombre + ", how are you doing today?");
 }
 
-showContent(1, greeting("Hugo"));
+saludar("Hugo");

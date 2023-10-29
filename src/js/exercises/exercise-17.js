246a8885f8639914d33970b5fce4e831971dcfcc
Hugo Lugo <hugolugoesperilla@gmail.com>
2023-10-29 22:21:12 +0100

    Ejercicio17

--- src/js/exercises/exercise-17.js
+++ src/js/exercises/exercise-17.js
@@ -4,3 +4,25 @@ Un isograma es una palabra o frase en la que cada letra aparece el mismo número
 Dado una cadena de texto, comprueba si es un isograma o no y devuelve un mensaje:
 "la cadena 'hola mundo' no es un isograma"
 */
+
+function esIsograma(cadena) {
+    cadena = cadena.replace(/ /g, "").toLowerCase();
+  
+    const frecuenciaLetras = {};
+  
+    for (let letra of cadena) {
+      if (frecuenciaLetras[letra]) {
+        return `La cadena '${cadena}' no es un isograma`;
+      }
+      frecuenciaLetras[letra] = 1;
+    }
+  
+    return `La cadena '${cadena}' es un isograma`;
+  }
+  
+  const cadena1 = "hola mundo";
+  const cadena2 = "murcielago";
+  
+  console.log(esIsograma(cadena1));
+  console.log(esIsograma(cadena2));
+  

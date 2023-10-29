d02917919644e4a4aee8f0ab54695c650c12e24f
Hugo Lugo <hugolugoesperilla@gmail.com>
2023-10-29 22:02:54 +0100

    Ejercicio11

--- src/js/exercises/exercise-11.js
+++ src/js/exercises/exercise-11.js
@@ -2,3 +2,18 @@
 EJERCICIO 11:
 Ordena las peliculas por valoracion (propiedad "rating") de mayor a menor
 */
+
+function compararPorRating(a, b) {
+    const ratingA = a.rating;
+    const ratingB = b.rating;
+  
+    if (ratingA > ratingB) return -1;
+    if (ratingA < ratingB) return 1;
+    return 0;
+  }
+  
+  const peliculasOrdenadasPorRating = movies.slice().sort(compararPorRating);
+  
+  console.log("Películas ordenadas por rating de mayor a menor:");
+  console.log(peliculasOrdenadasPorRating);
+  

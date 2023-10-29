8df032a0068290eb605257657c8225e0b2e84320
Hugo Lugo <hugolugoesperilla@gmail.com>
2023-10-29 22:14:19 +0100

    Ejercicio16

--- src/js/exercises/exercise-16.js
+++ src/js/exercises/exercise-16.js
@@ -1,4 +1,15 @@
 /* 
 EJERCICIO 16:
-Dado un año como parámetro (número), devuelve el número del siglo al que pertenece.
+Dado un año como parámetro (número), devuelve el número del century al que pertenece.
 */
+
+function centuryoftheyear(year) {
+    const century = Math.ceil(year / 100);
+    return century;
+  }
+  
+
+  const year = 2023;
+  const century = centuryoftheyear(year);
+  console.log(`El año ${year} pertenece al siglo ${century}`);
+  

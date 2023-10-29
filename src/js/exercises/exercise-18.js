ece22e080d9a4db7ff97cae55a3e07621f638b25
Hugo Lugo <hugolugoesperilla@gmail.com>
2023-10-29 22:23:00 +0100

    Ejercicio18

--- src/js/exercises/exercise-18.js
+++ src/js/exercises/exercise-18.js
@@ -5,3 +5,14 @@ devuelve dicha cadena transformada en hashtag, es decir, la cadena comenzando
 por almohadilla (#) y las palabras sin espacios comenzando la primera letra en mayúscula
 Ejemplo: "Programando en Javascript" se transforma en "#ProgramandoEnJavascript"
 */
+function textoAHashtag(cadena) {
+    const palabras = cadena.split(" ");
+  
+    const hashtag = palabras.map((palabra) => palabra.charAt(0).toUpperCase() + palabra.slice(1)).join("");
+  
+    return `#${hashtag}`;
+  }
+  
+  const cadena = "Programando en Javascript";
+  const hashtag = textoAHashtag(cadena);
+  console.log(hashtag); 

8e6bc4209ef755b640b7989d38314a34d1ef0f8a
Hugo Lugo <hugolugoesperilla@gmail.com>
2023-10-29 21:56:39 +0100

    Ejercicio9

--- src/js/exercises/exercise-9.js
+++ src/js/exercises/exercise-9.js
@@ -3,3 +3,19 @@ EJERCICIO 9:
 Genera un array de 10 números aleatorios entre 0 y 100, calcula el menor y
 el mayor y muestra el resultado por pantalla
 */
+// Función para generar un número aleatorio entre min y max (incluyendo ambos)
+function generarNumeroAleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const numerosAleatorios = [];
+for (let i = 0; i < 10; i++) {
+    numerosAleatorios.push(generarNumeroAleatorio(0, 100));
+}
+
+const numeroMenor = Math.min(...numerosAleatorios);
+const numeroMayor = Math.max(...numerosAleatorios);
+
+console.log("Números aleatorios:", numerosAleatorios);
+console.log("El número menor es:", numeroMenor);
+console.log("El número mayor es:", numeroMayor);

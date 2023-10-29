02ec56ed396c5ef3d898b2932190b74082db94ea
Hugo Lugo <hugolugoesperilla@gmail.com>
2023-10-29 21:48:46 +0100

    Ejercicio7

--- src/js/exercises/exercise-7.js
+++ src/js/exercises/exercise-7.js
@@ -5,4 +5,21 @@
 * es un palíndromo y devuelva un mensaje con la cadena de 
 * texto y el resultado
 */
+function esPalindromo(cadena) {
+    cadena = cadena.replace(/ /g, "").toLowerCase();
+
+    const cadenaInvertida = cadena.split("").reverse().join("");
+
+    if (cadena === cadenaInvertida) {
+        return `La cadena "${cadena}" es un palíndromo.`;
+    } else {
+        return `La cadena "${cadena}" no es un palíndromo.`;
+    }
+}
+
+const cadena1 = "anilina";
+const cadena2 = "hola mundo";
+
+console.log(esPalindromo(cadena1)); // Resultado: La cadena "anilina" es un palíndromo.
+console.log(esPalindromo(cadena2)); // Resultado: La cadena "hola mundo" no es un palíndromo.
 

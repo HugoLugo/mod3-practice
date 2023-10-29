0ecd05abf6f20e6797eed3da5992c5a422ec068a
Hugo Lugo <hugolugoesperilla@gmail.com>
2023-10-29 22:04:36 +0100

    Ejercicio12

--- src/js/exercises/exercise-12.js
+++ src/js/exercises/exercise-12.js
@@ -3,3 +3,13 @@ EJERCICIO 12:
 Filtra las peliculas que pertenezcan a la categoria "Drama" e incluye solo el título y la descripción
 Sugerencia: puedes usar la variable "categories" en el archivo "src/js/data/movies.js"
 */
+
+const peliculasDrama = movies
+  .filter((pelicula) => pelicula.categories.includes("Drama"))
+  .map((pelicula) => ({
+    title: pelicula.title,
+    description: pelicula.description,
+  }));
+
+console.log("Películas de Drama con título y descripción:");
+console.log(peliculasDrama);
